5c3e4b7509849f83d6f5c0d69f68f12a81fdcc34
Xavier <xravat@gmail.com>
2025-01-30 12:24:08 +0100

    Réalisation du js pour le formulaire, mais quelquechose ne va pas...à revoir. Diaporama en attendant.

---
+++ app.js
@@ -0,0 +1,35 @@
+function validerForm(){
+    const userName = document.getElementById("username").value
+    const email = document.getElementById("email").value
+    const message = document.getElementById("message").value
+
+    const errorUserName = document.getElementById("error_username")
+    const errorEmail = document.getElementById("error_email")
+    const errorMessage = document.getElementById("error_message")
+
+    errorUserName.innerHTML = ""
+    errorEmail.innerHTML = ""
+    errorMessage.innerHTML = ""
+
+    let isValid = true 
+
+    if (userName===""){
+        errorUserName.innerHTML = "Le nom est obligatoire."
+        isValid = false
+    }
+    if (!email.includes("@")){
+        errorEmail.innerHTML = "L'email doit contenir un @."
+        isValid = false
+    } else if (email===""){
+        errorEmail.innerHTML = "L'email est obligatoire."
+        isValid = false
+    }
+    if (message===""){
+        errorMessage.innerHTML = "Le message est obligatoire."
+        isValid = false
+    }
+    if (isValid){
+        alert("Le formulaire a été envoyé avec succès!")
+    }
+    return isValid
+}
